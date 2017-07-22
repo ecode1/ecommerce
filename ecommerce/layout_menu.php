92dd068011071131dbabf8db874006ab89c8849e
Joshua003 <30274838+Joshua003@users.noreply.github.com>
2017-07-22 12:40:43 -0400

    Login

--- ecommerce/layout_menu.php
+++ ecommerce/layout_menu.php
@@ -3,9 +3,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/estilo.css">
+  <link rel="stylesheet" href="../css/estilo.css">  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/funciones.js"></script>
 
 </head>
 <body>
@@ -22,8 +23,8 @@
 	      <li><a href="#">Contacto</a></li>	      
 	    </ul>
 	    <ul class="nav navbar-nav navbar-right">
-	      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	      <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 	    </ul>
 	  </div>
 	</nav>
