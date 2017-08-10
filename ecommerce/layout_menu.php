fe9b20a66a196da4dca845df2307c352b32b6cb8
ecode1 <eliaszamorano96@gmail.com>
2017-08-10 12:34:21 -0400

    menu responsivo

--- ecommerce/layout_menu.php
+++ ecommerce/layout_menu.php
@@ -12,19 +12,33 @@
 <body>
 
 <div class="container">
+
 	<nav class="navbar navbar-inverse">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <a class="navbar-brand" href="index.php?p=in">GamesOdyssey</a>
-	    </div>
-	    <ul class="nav navbar-nav">
-	      <li <?php if($_GET['p'] == 'ca'): ?> class="active"<?php endif ?> ><a href="catalogo.php?p=ca">Catalogo</a></li>
-	      <li <?php if($_GET['p'] == 'no'): ?> class="active"<?php endif ?> ><a href="#">Nosotros</a></li>
-	      <li <?php if($_GET['p'] == 'co'): ?> class="active"<?php endif ?> ><a href="#">Contacto</a></li>	      
-	    </ul>
-	    <ul class="nav navbar-nav navbar-right">
-	      <li <?php if($_GET['p'] == 'su'): ?> class="active"<?php endif ?>><a href="register.php?p=su"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	      <li <?php if($_GET['p'] == 'lo'): ?> class="active"<?php endif ?>><a href="login.php?p=lo"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-	    </ul>
-	  </div>
+		<div class="logo"></div>
+		
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar top-bar"></span>
+					<span class="icon-bar middle-bar"></span>
+					<span class="icon-bar bottom-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php?p=in">GamesOdyssey</a>
+			</div>
+			
+			<div id="navbar-1" class="navbar-collapse collapse mainnav">
+				<ul class="nav navbar-nav">
+			      <li <?php if($_GET['p'] == 'ca'): ?> class="active"<?php endif ?> ><a href="catalogo.php?p=ca">Catalogo</a></li>
+			      <li <?php if($_GET['p'] == 'no'): ?> class="active"<?php endif ?> ><a href="#">Nosotros</a></li>
+			      <li <?php if($_GET['p'] == 'co'): ?> class="active"<?php endif ?> ><a href="#">Contacto</a></li>	      
+			    </ul>
+			    <ul class="nav navbar-nav navbar-right">
+			      <li <?php if($_GET['p'] == 'su'): ?> class="active"<?php endif ?>><a href="register.php?p=su"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+			      <li <?php if($_GET['p'] == 'lo'): ?> class="active"<?php endif ?>><a href="login.php?p=lo"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			    </ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+		<!--/.container-fluid -->
 	</nav>
+	
